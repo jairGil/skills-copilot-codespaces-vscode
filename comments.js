00272b77ecb55d5e47a06b74a2ab1ff523392bf2
jairGil <61199734+jairGil@users.noreply.github.com>
2023-09-08 19:57:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,60 +1,53 @@
 // // Create a web server
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const port = 3000;
-
-// // Middleware
-// app.use(bodyParser.json());
-
-// // Data
-// let comments = [
-//   { id: 1, username: 'tj', comment: 'hello world' },
-//   { id: 2, username: 'tj', comment: 'hello world' },
-//   { id: 3, username: 'tj', comment: 'hello world' },
-//   { id: 4, username: 'tj', comment: 'hello world' },
-//   { id: 5, username: 'tj', comment: 'hello world' },
-//   { id: 6, username: 'tj', comment: 'hello world' },
-//   { id: 7, username: 'tj', comment: 'hello world' },
-//   { id: 8, username: 'tj', comment: 'hello world' },
-//   { id: 9, username: 'tj', comment: 'hello world' },
-//   { id: 10, username: 'tj', comment: 'hello world' },
-// ];
-
-// // GET /comments
-// app.get('/comments', (req, res) => {
-//   res.status(200).json(comments);
-// });
-
-// // POST /comments
-// app.post('/comments', (req, res) => {
-//   const comment = req.body;
-//   comment.id = comments.length + 1;
-//   comments.push(comment);
-//   res.status(201).json(comment);
-// });
-
-// // PUT /comments/:id
-// app.put('/comments/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const comment = req.body;
-//   comments = comments.map((comment) => {
-//     if (comment.id === id) {
-//       return { ...comment, ...req.body };
-//     }
-//     return comment;
-//   });
-//   res.status(200).json(comment);
-// });
-
-// // DELETE /comments/:id
-// app.delete('/comments/:id', (req, res) => {
-//   const id = parseInt(req.params.id);
-//   comments = comments.filter((comment) => comment.id !== id);
-//   res.status(204).json({ success: true });
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url');
+// const comments = [];
+// const app = http.createServer(function (request, response) {
+//   const parsedUrl = url.parse(request.url, true);
+//   const pathname = parsedUrl.pathname;
+//   const queryData = parsedUrl.query;
+//   if (pathname === '/') {
+//     fs.readFile('./index.html', function (err, description) {
+//       let template = description.toString();
+//       let list = templateList();
+//       let html = template.replace(/list/g, list);
+//       response.writeHead(200);
+//       response.end(html);
+//     });
+//   } else if (pathname === '/create') {
+//     let body = '';
+//     request.on('data', function (data) {
+//       body = body + data;
+//     });
+//     request.on('end', function () {
+//       const post = qs.parse(body);
+//       const title = post.title;
+//       const description = post.description;
+//       fs.writeFile(`./data/${title}`, description, 'utf8', function (err) {
+//         response.writeHead(302, { Location: `/?id=${title}` });
+//         response.end();
+//       });
+//     });
+//   } else if (pathname === '/update') {
+//     fs.readdir('./data', function (err, filelist) {
+//       const filteredId = path.parse(queryData.id).base;
+//       fs.readFile(`./data/${filteredId}`, 'utf8', function (err, description) {
+//         let title = queryData.id;
+//         let list = templateList(filelist);
+//         let html = templateHTML(title, list, `<form action="/update_process" method="post">
+//         <input type="hidden" name="id" value="${title}">
+//         <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+//         <p>
+//             <textarea name="description" placeholder="description">${description}</textarea>
+//         </p>
+//         <p>
+//             <input type="submit">
+//         </p>
+//         </form>`);
+//         response.writeHead(200);
+//         response.end(html);
+//       });
+//     });
+//   } else if (pathname === '/update_process') {
+//     let body =
